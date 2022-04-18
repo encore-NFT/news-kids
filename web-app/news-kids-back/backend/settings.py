3441015fbac5e185d4697e0de0990f748b854ce2
jeon-jeongin <wjddls1702@naver.com>
2022-04-18 18:04:07 +0900

    FIX CORS localhost

--- web-app/news-kids-back/backend/settings.py
+++ web-app/news-kids-back/backend/settings.py
@@ -60,8 +60,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
+# CORS 허용 여부 True
+CORS_ORIGIN_ALLOW_ALL = False
 
 ROOT_URLCONF = 'backend.urls'
 
