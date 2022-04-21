688e7d7afcef87854f312049222210ffe503df63
jeon-jeongin <wjddls1702@naver.com>
2022-04-22 00:07:44 +0900

    FIX errors message

--- web-app/news-kids-front/src/screens/SignUp.js
+++ web-app/news-kids-front/src/screens/SignUp.js
@@ -13,7 +13,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 function SignUp() {
     const navigate = useNavigate();
-    const { register, handleSubmit, formState: { errors }, setError } = useForm({
+    const { register, handleSubmit, formState: { errors }, setError, clearErrors } = useForm({
         mode: "onChange",
     });
 
@@ -35,6 +35,9 @@ function SignUp() {
         }
     }
 
+    const clearLoginError = () => {
+        clearErrors("result");
+    }
     return (
         <AuthLayout>
             <FormBox>
@@ -46,13 +49,14 @@ function SignUp() {
                         {...register('name', {
                             required: "사용자 아이디는 필수입니다."
                         })}
+                        onChange={clearLoginError}
                         name="name"
                         label="아이디"
                         type="text"
                         variant="outlined"
                         size="small"
                     />
-                    {errors.name && (<FormError message={errors.name.message} />)}
+                    {errors?.name && (<FormError message={errors?.name?.message} />)}
                     <AuthInput
                         {...register('nickname')}
                         name="nickname"
@@ -69,13 +73,14 @@ function SignUp() {
                                 message: "이메일 형식이 아닙니다."
                             },
                         })}
+                        onChange={clearLoginError}
                         name="email"
                         label="이메일"
                         type="text"
                         variant="outlined"
                         size="small"
                     />
-                    {errors.email && (<FormError message={errors.email.message} />)}
+                    {errors?.email && (<FormError message={errors?.email?.message} />)}
                     <AuthInput
                         {...register('password', {
                             required: "비밀번호는 필수입니다.",
@@ -84,15 +89,16 @@ function SignUp() {
                                 message: "비밀번호는 최소 8자 이상입니다."
                             },
                         })}
+                        onChange={clearLoginError}
                         name="password"
                         label="비밀번호"
                         type="password"
                         variant="outlined"
                         size="small"
                     />
-                    {errors.password && (<FormError message={errors.password.message} />)}
+                    {errors?.password && (<FormError message={errors?.password?.message} />)}
                     <AuthButton type="submit">회원가입</AuthButton>
-                    <FormError message={errors?.result?.message} />
+                    {errors?.result && (<FormError message={errors?.result?.message} />)}
                 </form>
             </FormBox>
             <BottomBox cta="계정이 있으신가요?" linkText="로그인" link={`/login`} />
