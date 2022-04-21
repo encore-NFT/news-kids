ba504be3326617cacae84727d73f8120ee0a8086
sheon-j <jsh7704@naver.com>
2022-04-21 21:33:45 +0900

    ADD wordcount history

--- web-app/news-kids-back/word_count/urls.py
+++ web-app/news-kids-back/word_count/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views import (
     WordCountView,
-    PastWordCountView
+    PastWordCountView,
+    WordCountDetailView,
 )
 
 urlpatterns = [
     # 워드카운트
     path('', WordCountView.as_view()),
-    path('<str:past_week>', PastWordCountView.as_view()),
+    path('week/<str:past_week>', PastWordCountView.as_view()),
+    path('search', WordCountDetailView.as_view()),
 ]
