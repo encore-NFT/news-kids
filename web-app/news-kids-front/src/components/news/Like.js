7cfe9c1dd617571fb0404f61034114690887036e
jeon-jeongin <wjddls1702@naver.com>
2022-04-22 14:00:20 +0900

    FIX like component

--- web-app/news-kids-front/src/components/news/Like.js
+++ web-app/news-kids-front/src/components/news/Like.js
@@ -1,27 +1,20 @@
 import { IconButton, Typography } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-function Like({newsId, likeCount, likeStatus}) {
-    const ButtonHandler = (e) => {
-        likeStatus = !likeStatus;
-        console.log(likeStatus?"true":"false")
+function Like({ newsId, likeCount, likeStatus }) {
+
+    const [toggleLike, setToggleLike] = useState(likeStatus);
+
+    const ButtonHandler = () => {
+        setToggleLike(likeStatus => !likeStatus)
     }
 
-    // const likeButton = () => {
-    //     likeStatus ? <FavoriteIcon fontSize='large'/> : <FavoriteBorderIcon fontSize='large'/>
-    // }
-
-    const likeButton = likeStatus ? <FavoriteIcon fontSize='large'/> : <FavoriteBorderIcon fontSize='large'/>
-    useEffect=(()=>{
-        likeButton
-    })
-    
     return (
         <>
             <IconButton color='secondary' onClick={ButtonHandler}>
-                {likeButton}
+                {toggleLike ? <FavoriteIcon fontSize='large' /> : <FavoriteBorderIcon fontSize='large' />}
             </IconButton>
         </>
     )
