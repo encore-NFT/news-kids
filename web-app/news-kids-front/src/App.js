1d2c420aedf5c329b2a72d20963bce26d3a06999
jeon-jeongin <wjddls1702@naver.com>
2022-04-22 17:41:08 +0900

    ADD comment profile page

--- web-app/news-kids-front/src/App.js
+++ web-app/news-kids-front/src/App.js
@@ -10,6 +10,7 @@ import Trend from "./screens/Trend";
 import AccountRoutes from "./routes/AccountRoutes"
 import { GlobalStyles } from "./styles";
 import Search from "./screens/Search";
+import CommentProfile from "./screens/CommentProfile";
 
 function App() {
 
@@ -25,6 +26,7 @@ function App() {
           <Route path={`/trend`} element={<Layout setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} ><Trend /></Layout>} />
           <Route path={`/quiz`} element={<Layout setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} ><Quiz /></Layout>} />
           <Route path={`/profile`} element={<Layout setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn}><Profile /></Layout>} />
+          <Route path={`/profile/:user_name`} element={<Layout setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn}><CommentProfile /></Layout>} />
           <Route path={`/search`} element={<Layout setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn}><Search /></Layout>} />
           <Route path={`/accounts/*`} element={<AccountRoutes setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />} />
           <Route path={`/login`} element={<Login setIsLoggedIn={setIsLoggedIn} />} />
