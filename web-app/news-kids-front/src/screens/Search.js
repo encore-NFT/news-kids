b0bf7a5a3d9424ed21628a7d148a9d3d0792c7bf
jeon-jeongin <wjddls1702@naver.com>
2022-04-22 12:53:02 +0900

    ADD search header

--- web-app/news-kids-front/src/screens/Search.js
+++ web-app/news-kids-front/src/screens/Search.js
@@ -5,12 +5,15 @@ import ContainerLayout from '../components/shared/ContainerLayout';
 import ContentLayout from '../components/shared/ContentLayout';
 import { Grid, Paper, styled, Typography } from '@material-ui/core';
 import Thumbnail from '../components/profile/Thumbnail';
+import UnderLine from '../components/shared/UnderLine';
+import styledComponent from 'styled-components';
 
 function Search() {
     const location = useLocation();
     const data = location.state;
 
     const [searchData, setSearchData] = useState("");
+    const newsSum = Object.keys(searchData).length;
 
     const postSearch = async (data) => {
         try {
@@ -30,9 +33,13 @@ function Search() {
 
     return (
         <>
-            {searchData ?
+            {searchData && newsSum !== 0 ?
                 <ContainerLayout>
-                    < ContentLayout >
+                    < ContentLayout>
+                        {data.word ?
+                            <Typography variant='h5' component="h1"><span style={{ color: "#4d88d8" }}>{data?.word}</span> 검색결과 {newsSum}건</Typography> :
+                            <Typography variant='h5' component="h1">전체 검색결과 {newsSum}건</Typography>}
+                        <UnderLine />
                         {searchData?.map((search) => (
                             <Link to={`/news/${search.news_id}`} key={search.news_id}>
                                 <CommentContainers>
@@ -51,8 +58,16 @@ function Search() {
                         ))
                         }
                     </ContentLayout >
-                </ContainerLayout > :
-                null}
+                </ContainerLayout >
+                :
+                <ContainerLayout>
+                    < ContentLayout>
+                        <ErrorTitle variant='h6' component="h2"><span style={{ color: "#ff5f4e" }}>'{data?.word}'</span>에 대한 검색결과가 없습니다.</ErrorTitle>
+                        <ErrorContent variant='body2'>모든 단어의 철자가 정확한지 확인해 보세요.</ErrorContent>
+                        <ErrorContent variant='body2'>다른 검색어를 사용해보세요.</ErrorContent>
+                        <ErrorContent variant='body2'>보다 일반적인 검색어로 다시 검색해 보세요.</ErrorContent>
+                    </ContentLayout >
+                </ContainerLayout >}
         </>
     )
 }
@@ -86,3 +101,14 @@ const CommentContainers = styled(Paper)({
     border: '0.5px solid #eeeeee',
     minWidth: '200px'
 })
+
+const ErrorTitle = styled(Typography)({
+    textAlign: 'left',
+    marginBottom: '30px',
+})
+
+const ErrorContent = styledComponent.li`
+    text-align: left;
+    margin: 0px 0px 5px 10px;
+    font-size: 14px
+`
