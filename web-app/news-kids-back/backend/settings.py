45744fb975a3731fbc6fbb0d4f6af0c3a9b28ffd
sheon-j <jsh7704@naver.com>
2022-04-28 13:30:32 +0900

    set backend allowed host

--- web-app/news-kids-back/backend/settings.py
+++ web-app/news-kids-back/backend/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000', 'http://127.0.0.1:3000'
 ]
 # CORS 허용 여부 True
 CORS_ORIGIN_ALLOW_ALL = False
